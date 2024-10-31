c564e730643244b7fd360e84833bc621d73c3dbc
nabiladwy <nabiladwy10@gmail.com>
2024-10-31 09:22:29 +0700

    nabila

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -4,4 +4,11 @@ $db = 'chat_app';
 $user = 'root';
 $pass = 'nabila123';
 $charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$option = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 ?>
