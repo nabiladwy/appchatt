28debfe6907955a1f24e6134c6f5de563cf4cfba
salwasuci <salwasuci57@gmail.com>
2024-10-31 10:43:02 +0700

    salwa

--- modul/update_user.php
+++ modul/update_user.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hai
-</body>
-</html>
+<?php
+require 'koneksi.php';
+
+$id = $_GET['id'];
+$stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+$stmt->execute([$id]);
+$user = $stmt->fetch();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $username = $_post['username'];
+    $email = $_POST['email'];
+
+    $stmt = $pdo->prepare("UPDATE users SET username = ?, email = ? WHERE id = ?");
+    $stmt->execute([$username, $email, $id]);
+
+    header("Location: read_users.php");
+}
+?>
+<form method="POST">
+    Username: <input type="text" name="username" value="<?= $user['username'] ?>" required>
+    Email: <input type="email" name="email" value="<?= $user['email'] ?>" required>
+    <button type="submit">Update</button>
+</form>
