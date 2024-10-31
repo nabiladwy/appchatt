2b5b7b89e7d440a517113a80d84fc6843862a919
salwasuci <salwasuci57@gmail.com>
2024-10-31 10:48:22 +0700

    salwa

--- modul/delete_user.php
+++ modul/delete_user.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+require 'lib/koneksi.php';
+
+$id = $_GET['id'];
+$stmt - $pdo->prepare("DELETE FROM users WHERE id = ?");
+$stmt->execute([$id]);
+
+header ("Location: read_users.php");
+?>
