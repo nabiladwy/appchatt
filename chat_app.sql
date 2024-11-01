97d06adb596887619dc6b06962bdfa6e787a9a4e
salwasuci <salwasuci57@gmail.com>
2024-11-01 15:30:50 +0700

    salwa

--- chat_app.sql
+++ chat_app.sql
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `chats` (
-  `id` int(11) NOT NULL,
+  `id` int AUTO_INCREMENT primary KEY,
   `chat_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
