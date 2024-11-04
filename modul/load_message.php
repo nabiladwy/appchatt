7dcd120b2b0641add6f70962b51bfe1999c257d5
Your Name <you@example.com>
2024-11-04 11:32:34 +0700

    kia

--- modul/load_message.php
+++ modul/load_message.php
@@ -2,10 +2,10 @@
 require'koneksi.php';
 
 $chat_id=$_GET['chat_id'];
-$stmt=$pdo->prepare("SELECTmessage.*,users.usernameFROMmessageJOINusersONmessage.user_id=users.idWHEREchat_id=?");
+$stmt=$pdo->prepare("SELECT message.*,users.username FROM message JOIN users ON message.user_id=users.idWHEREchat_id=?");
 $stmt->execute([$chat_id]);
 $message=$smt->fetchAll();
 
 foreach($message as $message){
-    echo"<div><strong>{$message['username']:</strong>{$message['message']}<em>{$message['created_at']}</em></div>";
+    echo"<div><strong>{$message['username']} :</strong> {$message['message']} <em>{$message['created_at']}</em></div> ";
 }
