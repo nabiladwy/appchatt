e1dc91a712b69d42d8f3f28cd34e6a5d15a83bf0
nabiladwy <nabiladwy10@gmail.com>
2024-11-04 11:43:24 +0700

    nabila

--- modul/load_message.php
+++ modul/load_message.php
@@ -2,10 +2,11 @@
 require'koneksi.php';
 
 $chat_id=$_GET['chat_id'];
-$stmt=$pdo->prepare("SELECTmessage.*,users.usernameFROMmessageJOINusersONmessage.user_id=users.idWHEREchat_id=?");
+$stmt=$pdo->prepare("SELECT message.*,users.username FROM messages JOIN users ON messages.user_id=users.id WHERE chat_id=?");
 $stmt->execute([$chat_id]);
-$message=$smt->fetchAll();
+$messages = $stmt->fetchAll();
 
-foreach($message as $message){
-    echo"<div><strong>{$message['username']:</strong>{$message['message']}<em>{$message['created_at']}</em></div>";
+foreach($messages as $message){
+    echo"<div><strong>{$message['username']:</strong>{$message['message']}
+    <em>{$message['created_at']}</em></div>";
 }
