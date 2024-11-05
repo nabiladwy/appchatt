a79c9414d2e9e7536a7011dda57aeead89143455
salwasuci <salwasuci57@gmail.com>
2024-11-05 08:37:46 +0700

    wawa

--- modul/default.php
+++ modul/default.php
@@ -1,35 +1,106 @@
-<div class="container-fluid p-5 bg-warning text-white text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Jangan lupa bayar Kas <?=strtoupper($result['nama'])?></p> 
-  <a href="?page=keluar" class="btn btn-danger">Logout</a>
-</div>
+<div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+<div class="col-md-4">
+             
+              <div class="box box-warning direct-chat direct-chat-warning">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Chat Messages</h3>
 
+                  <div class="box-tools pull-right">
+                    <span data-toggle="tooltip" title="" class="badge bg-yellow" data-original-title="3 New Messages">20</span>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="" data-widget="chat-pane-toggle" data-original-title="Contacts">
+                      <i class="fa fa-comments"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+              
+                <div class="box-body">
+                  
+                  <div class="direct-chat-messages">
+                
+                    <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">Timona Siera</span>
+                        <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                      </div>
+                     
+                      <img class="direct-chat-img" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="message user image">
+                   
+                      <div class="direct-chat-text">
+                        For what reason would it be advisable for me to think about business content?
+                      </div>
+        
+                    </div>
+                    
+                    <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">Sarah Bullock</span>
+                        <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
+                      </div>
+                     
+                      <img class="direct-chat-img" src="https://img.icons8.com/office/36/000000/person-female.png" alt="message user image">
+                      
+                      <div class="direct-chat-text">
+                        Thank you for your believe in our supports
+                      </div>
+                   
+                    </div>
+                   
 
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-sm-3">
-      <h3>Data Tarif</h3>
-      <div class="" style="width:200px">
-      <a href="?page=tarif"><img class="card-img-top " src="img/lampp.png" alt="Card image"></a>
-    </div>
-    </div>
-    <div class="col-sm-3">
-      <h3>Data Clients</h3>
-      <div class="" style="width:200px">
-       <a href="?page=client"> <img class="card-img-top " src="img/clii.png" alt="Card image"></a>
-    </div>  
-    </div>
-    <div class="col-sm-3">
-      <h3>Data Payments</h3>        
-      <div class="" style="width:200px">
-       <a href="?page=payment"> <img class="card-img-top " src="img/mon.png" alt="Card image"></a>
-    </div>   
-    </div>
-    <div class="col-sm-3">
-      <h3>Data Users</h3>        
-      <div class="" style="width:200px">
-       <a href="?page=user"> <img class="card-img-top " src="img/usee.png" alt="Card image"></a>
-    </div>  
-    </div>
-  </div>
-</div>
+                  
+                    <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">Timona Siera</span>
+                        <span class="direct-chat-timestamp pull-right">23 Jan 5:37 pm</span>
+                      </div>
+             
+                      <img class="direct-chat-img" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="message user image">
+                     
+                      <div class="direct-chat-text">
+                        For what reason would it be advisable for me to think about business content?
+                      </div>
+                    
+                    </div>
+                   
+                    <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">Sarah Bullock</span>
+                        <span class="direct-chat-timestamp pull-left">23 Jan 6:10 pm</span>
+                      </div>
+                     
+                      <img class="direct-chat-img" src="https://img.icons8.com/office/36/000000/person-female.png" alt="message user image">
+          
+                      <div class="direct-chat-text">
+                        I would love to.
+                      </div>
+                    
+                    </div>
+            
+
+                  </div>
+                 
+                </div>
+               
+                <div class="box-footer">
+                  <form action="#" method="post">
+                    <div class="input-group">
+                      <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                      <span class="input-group-btn">
+                            <button type="button" class="btn btn-warning btn-flat">Send</button>
+                          </span>
+                    </div>
+                  </form>
+                </div>
+             
+              </div>
+           
+            </div>
+             </div>
+            
+              </div>
+             
+            </div>
