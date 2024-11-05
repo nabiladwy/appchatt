ed2637383493fca519caaada10b93146ea1d6055
salwasuci <salwasuci57@gmail.com>
2024-11-05 07:52:03 +0700

    salwa

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -6,15 +6,15 @@ $pass = 'majumapan';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$option = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+$options = [
+    PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES         => false,
 ];
 
-try {
+try{
     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e)  {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+} catch (\PDOException $e) {
+throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
